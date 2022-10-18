8c12e3fa69cee4ef6623a99b0a060cca63a2e5d0
deg <david.deguara@mcast.edu.mt>
2022-10-18 11:33:39 +0200

    Adding Pause functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject _canvas;
+    private bool _isPaused;
+    
+    private void Start()
     {
-        
+        _canvas = GameObject.Find("Canvas");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("P"))
+        {
+            _isPaused = !_isPaused;
+            if (_isPaused)
+            {
+                //display canvas
+            }
+            else
+            {
+                //hide canvas
+            }
+        }  
     }
 }
