8f7292114c41a835fc2805d9e0effc1bcb585dde
deg <david.deguara@mcast.edu.mt>
2022-10-18 11:44:20 +0200

    Added Pause UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,31 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    private GameObject _canvas;
-    private bool _isPaused;
+    private Image _pauseScreen;
+    private bool _isPaused = false;
+    private bool _isPausable = true;
     
     private void Start()
     {
-        _canvas = GameObject.Find("Canvas");
+        _pauseScreen = GameObject.Find("Canvas").GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("P"))
+        if (Input.GetKey(KeyCode.P) && _isPausable)
         {
+            //prevent additional pauses for a certain period of time
+            _isPausable = false;
+            //start a timer to re-enable isPausable after 2s
+            StartCoroutine(ReEnablePause(0.5f));
+            
+            //change pause state
             _isPaused = !_isPaused;
             if (_isPaused)
             {
                 //display canvas
+                _pauseScreen.enabled = true;
             }
             else
             {
                 //hide canvas
+                _pauseScreen.enabled = false;
             }
         }  
+    }
+
+    IEnumerator ReEnablePause(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        _isPausable = true;
     }
 }
